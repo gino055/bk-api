e5d0fef6cddb9cdcaea35a81afd35d5ef8a96651
Gino.Hsieh <gino055@dx-tech.tw>
2026-06-15 15:09:51 +0800

    init bk_ag

---
+++ app.js
@@ -0,0 +1,31 @@
+var os = require('os');
+var ip = '0.0.0.0';
+var ips = os.networkInterfaces();
+Object
+    .keys(ips)
+    .forEach(
+        function (_interface) {
+            ips[_interface]
+                .forEach(
+                    function (_dev) {
+                        if (_dev.family === 'IPv4' && !_dev.internal) {
+                            ip = _dev.address
+                        }
+                    }
+                )
+        }
+    );
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const site = process.env.site || 'unknown';
+const port = process.env.ag_port || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello World ${site}! v1.0.0, Host IP: ${ip}, Port: ${port}</h1>`);
+});
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
+});
